79e73accb7e6d21ce93465e7dda7c202ba4767eb
Jorane Membrez <jorane.membrez@outlook.com>
2023-10-09 21:42:24 +0200

    essaie gitignore

--- animals-place-api/animals-place/models/user.js
+++ animals-place-api/animals-place/models/user.js
@@ -2,10 +2,41 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 // Define the schema for users
 const userSchema = new Schema({
-  name: String,
-  address: String,
-  email: String,
-  password: String,
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  registrationDate: {
+    type: Date,
+    default: Date.now(),
+  },
+  address: {
+    type: String,
+    required: true,
+  },
 });
+
+userSchema.set("toJSON", {
+  transform: transformJsonUser,
+});
+// permet de supprimer le mot de passe dans le json renvoyé
+function transformJsonUser(doc, json, options) {
+  delete json.password;
+  return json;
+}
+
 // Create the model from the schema and export it
 export default mongoose.model("User", userSchema);
